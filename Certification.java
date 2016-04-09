10617849a95899f7c9a6b4df477cfb3027d3fff3
dvdlin <davidlin@uga.edu>
2016-04-09 18:51:11 -0400

    Certification

---
+++ Certification.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Certification {
+
+	
+	
+	private int certID;
+	private String certName;
+	private String certSponsor;
+	private String certExpiry;
+	/**
+	 * @return the certID
+	 */
+	public int getCertID() {
+		return certID;
+	}
+	/**
+	 * @param certID the certID to set
+	 */
+	public void setCertID(int certID) {
+		this.certID = certID;
+	}
+	/**
+	 * @return the certName
+	 */
+	public String getCertName() {
+		return certName;
+	}
+	/**
+	 * @param certName the certName to set
+	 */
+	public void setCertName(String certName) {
+		this.certName = certName;
+	}
+	/**
+	 * @return the certSponsor
+	 */
+	public String getCertSponsor() {
+		return certSponsor;
+	}
+	/**
+	 * @param certSponsor the certSponsor to set
+	 */
+	public void setCertSponsor(String certSponsor) {
+		this.certSponsor = certSponsor;
+	}
+	/**
+	 * @return the certExpiry
+	 */
+	public String getCertExpiry() {
+		return certExpiry;
+	}
+	/**
+	 * @param certExpiry the certExpiry to set
+	 */
+	public void setCertExpiry(String certExpiry) {
+		this.certExpiry = certExpiry;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Certification [certID=" + certID + ", certName=" + certName + ", certSponsor=" + certSponsor
+				+ ", certExpiry=" + certExpiry + "]";
+	}
+	
+	
+	
+	
+}
